c717a9a69626b888b770faf175614848a0b3bfaa
oskuhei <50679155+oskuhei@users.noreply.github.com>
2019-10-11 13:12:54 +0300

    Add files via upload

---
+++ Registration.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+export class Registrations extends Component {
+   
+    constructor(props) {
+        super(props);
+    }
+
+render() {
+    return (
+        <div>
+            <p>Registrations</p>
+        </div>
+    )
+}
+}
